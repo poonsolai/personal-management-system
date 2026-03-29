ed0085cd76a192e69dc5879aca7a1968e4ef7fdf
poonsolai <poonsolai05@gmail.com>
2026-03-29 12:29:47 +0530

    update morethen files its last commit for v-1.1

--- src/hooks/api.js
+++ src/hooks/api.js
@@ -1,4 +1,4 @@
-// const api = 'https://personal-management-system-backend.onrender.com';
-const api = 'http://localhost:3000';
+const api = 'https://personal-management-system-backend.onrender.com';
+// const api = 'http://localhost:3000';
 
 export default api;
